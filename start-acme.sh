0246f65eb8df7f585bbc969f0b32e1051d2f7342
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2026-03-06 15:45:33 +0100

    Add a function to start `fontsrv` in start-acme.sh

--- start-acme.sh
+++ start-acme.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
-FONTSRV=`ps | grep fontsrv | grep -v grep | awk '{print $5}'`
+__start_fontsrv()
+{
+	# If you have problems, such as the /mnt/font is esmpty or has not been created, try launching the command with the
+	# `-m` flag, like the following:
+	#     fontsrv -m /mnt/font
 
-if [ -z "$FONTSRV" ]; then
-    echo "Avvio in corso di fontsrv"
-    fontsrv &
-fi
+	FONTSRV=`ps | grep fontsrv | grep -v grep | awk '{print $5}'`
 
+	if [ -z "$FONTSRV" ]; then
+		echo 'Executing `fontsrv` in background...'
+		fontsrv &
+	fi
+}
+
+# If you need `fontsrv`, uncomment the following line:
+#__start_fontsrv
+
+#acme -a -f /mnt/font/'Courier'/9a/font &
 #acme -a -f /mnt/font/'DejaVuSansMono'/8a/font &
 acme -a -f /mnt/font/DejaVuSansMono/9a/font &
-#acme -a -f /mnt/font/'Courier'/9a/font &
 
 #echo ">search >find-class mvnwc -v |c |uc" | 9p write acme/new/tag
