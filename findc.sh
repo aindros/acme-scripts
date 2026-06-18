57edd5b288e9b37fd8e44ddab88725025c63e34c
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2026-06-18 17:31:55 +0200

    Simplify the findc.sh script

--- findc.sh
+++ findc.sh
@@ -1,32 +1,65 @@
 #!/bin/sh
 
-homedevel=`echo ~/devel/`
-directory=`9p read acme/index | awk '{print "|"$1" "$6}' | grep "|$winid " | awk '{print $2}'`
-directory=`echo $directory | sed 's|/[^/]*$||'`
+# This script find a Java Class inside the Maven project.
 
-if [ ! -z "`echo $directory | grep -o $homedevel`" ]; then
-    directory=`echo $directory | sed "s|$homedevel||"`
-    curdir=$homedevel
-    for i in `echo $directory | sed 's|/| |g'`; do
-        curdir=$curdir$i/
-        if [ -f "${curdir}pom.xml" ]; then
-            rootdir=$curdir
-            break
-        fi
-    done
+_get_dir()
+{
+	if [ ! -z $winid ]; then
+		9p read acme/index                 \
+				| awk '{print "|"$1" "$6}' \
+				| grep "|$winid "          \
+				| awk '{print $2}'         \
+				| sed 's|/[^/]*$||'        \
+				| grep -oE '/.*'
+	else
+		pwd
+	fi
+}
+
+_reverse()
+{
+	local rev=
+	for i in $@; do
+		rev="$i $rev"
+	done
+
+	echo $rev
+}
+
+_find_pom()
+{
+	# Navigating to the pom.xml
+	curdir=$directory
+	for i in $(_reverse $(echo $directory | sed 's/\// /g')); do
+		if [ -f "$curdir/pom.xml" ]; then
+			echo $curdir
+			return
+		fi
+		curdir=$(echo $curdir | sed "s/\/$i$//g")
+	done
+}
+
+_get_class_name()
+{
+	if [ -z $1 ]; then
+		IFS=''
+		while read -r LINE || [ -n "$LINE" ]; do
+			echo $LINE
+			return
+		done
+	else
+		echo $1
+	fi
+}
+
+directory=$(_get_dir)
+
+if [ -z "$directory" ]; then
+	echo "The directory is empty. Exit."
+	exit 1
 fi
 
-rootdir=${rootdir}src
+rootdir=$(_find_pom)
+classname=$(_get_class_name $1)
 
-if [ -z $1 ]; then
-    IFS=''
-    while read -r LINE || [ -n "$LINE" ]; do
-        FOUND=`find $rootdir -name "$LINE.java"`
-        echo $FOUND
-        exit
-    done
-else
-    find $rootdir -name "$1.java"
-fi
-
-
+find $rootdir -name $classname.java
